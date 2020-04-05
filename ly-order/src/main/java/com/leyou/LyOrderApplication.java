ff7a3bd3a8aa0e15d07343463cf2a5135537cbec
13100322280 <1092577929@qq.comcd>
2020-04-05 16:07:09 +0800

    一期项目

--- ly-order/src/main/java/com/leyou/LyOrderApplication.java
+++ ly-order/src/main/java/com/leyou/LyOrderApplication.java
@@ -11,9 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableFeignClients
 @MapperScan("com.leyou.order.mapper")
 public class LyOrderApplication {
-    //苏晓杰测试。。。
-    //杨伟烽测试
-    //方震测试·····
+
     public static void main(String[] args) {
         SpringApplication.run(LyOrderApplication.class, args);
     }
