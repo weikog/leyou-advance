7992047f30b067787ce2676d3321c17c8b7544c3
mylikr <mylikr@aliyun.com>
2020-04-06 23:04:04 +0800

    新增用户中心个人信息持久层

---
+++ ly-user/src/main/java/com/leyou/user/mapper/UserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.leyou.user.mapper;
+
+import com.leyou.common.mapper.BaseMapper;
+import com.leyou.user.entity.UserInfo;
+
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+}
