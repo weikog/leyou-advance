65cb661b955266f7a2fb6b703ecb65f6cd00cf73
suxiaojie <chrissu88@163.com>
2020-04-08 16:03:40 +0800

    增加查询首页导航菜单的业务代码

--- ly-item/src/main/java/com/leyou/item/service/CategoryService.java
+++ ly-item/src/main/java/com/leyou/item/service/CategoryService.java
@@ -1,14 +1,24 @@
 package com.leyou.item.service;
 
+import com.leyou.common.constant.LyConstants;
 import com.leyou.common.exception.pojo.ExceptionEnum;
 import com.leyou.common.exception.pojo.LyException;
+import com.leyou.common.utils.BeanHelper;
+import com.leyou.common.utils.JsonUtils;
+import com.leyou.item.dto.AllMenuDTO;
+import com.leyou.item.dto.CategoryDTO;
+import com.leyou.item.dto.MenuDTO;
 import com.leyou.item.entity.Category;
+import com.leyou.item.entity.CategoryGroup;
+import com.leyou.item.mapper.CategoryGroupMapper;
 import com.leyou.item.mapper.CategoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -17,6 +27,12 @@ public class CategoryService {
 
     @Autowired
     private CategoryMapper categoryMapper;
+    
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private CategoryGroupMapper categoryGroupMapper;
 
     public List<Category> findCategoryByPid(Long pid) {
         //封装条件
@@ -39,4 +55,89 @@ public class CategoryService {
         return list;
     }
 
+    //1级菜单组
+    public List<CategoryGroup> getCategoryGroups() {
+        List<CategoryGroup> categoryGroups = categoryGroupMapper.selectAll();
+
+        if (CollectionUtils.isEmpty(categoryGroups)){
+            throw new LyException(ExceptionEnum.CATEGORY_NOT_FOUND);
+        }
+        return categoryGroups;
+    }
+
+
+    //首页导航加载全部菜单
+    public List<AllMenuDTO> getAllMenu() {
+        List<CategoryGroup> categoryGroups = getCategoryGroups();
+        List<AllMenuDTO> allMenuDTOS = BeanHelper.copyWithCollection(categoryGroups, AllMenuDTO.class);
+        allMenuDTOS.forEach(allMenuDTO -> {
+            String[] cids = allMenuDTO.getCategoryIds().split(",");
+            List<MenuDTO> menuDTOS = new LinkedList<>();
+            for (int i = 0; i < cids.length; i++) {
+                List<Category> secondCategorys = findCategoryByPid(Long.valueOf(cids[i]));
+                secondCategorys.forEach(secondCategory ->{
+                    MenuDTO menuDTO = new MenuDTO();
+                    menuDTO.setId(secondCategory.getId());
+                    menuDTO.setName(secondCategory.getName());
+                    List<Category> categorys = findCategoryByPid(secondCategory.getId());
+                    List<CategoryDTO> thirCategorys = BeanHelper.copyWithCollection(categorys, CategoryDTO.class);
+                    menuDTO.setThirCategorys(thirCategorys);
+                    menuDTOS.add(menuDTO);
+                });
+            }
+            allMenuDTO.setMenuDTOS(menuDTOS);
+        });
+        return allMenuDTOS;
+    }
+    
+    /*public void addRedis(){
+        String indexMenuKey = LyConstants.INDEX_MENU_KEY;
+        BoundHashOperations<String, String, String> indexMenuMap = redisTemplate.boundHashOps(indexMenuKey);
+
+        List<AllMenuDTO> allMenuDTOS = getAllMenu();
+        allMenuDTOS.forEach(allMenuDTO -> {
+            indexMenuMap.put(allMenuDTO.getGroupId().toString() , JsonUtils.toString(allMenuDTO));
+        });
+
+    }*/
+
+    /**
+     * 添加首页菜单数据到redis
+     */
+    public void addRedis(){
+        String indexMenuKey = LyConstants.INDEX_MENU_KEY;
+        List<AllMenuDTO> allMenuDTOS = getAllMenu();
+        redisTemplate.opsForValue().set(indexMenuKey,JsonUtils.toString(allMenuDTOS));
+
+    }
+
+
+    /*public List<AllMenuDTO> findMenu(){
+        String indexMenuKey = LyConstants.INDEX_MENU_KEY;
+        BoundHashOperations<String, String, String> indexMenuMap = redisTemplate.boundHashOps(indexMenuKey);
+        List<String> menus = indexMenuMap.values();
+        if (CollectionUtils.isEmpty(menus)){
+            throw new LyException(ExceptionEnum.CATEGORY_NOT_FOUND);
+        }
+        List<AllMenuDTO> allMenuDTOS = menus.stream()
+                .map(menu -> JsonUtils.toBean(menu, AllMenuDTO.class))
+                .collect(Collectors.toList());
+        return allMenuDTOS;
+    }*/
+
+    /**
+     * 在redis中查找首页菜单数据
+     * @return
+     */
+    public List<AllMenuDTO> findMenu(){
+        String indexMenuKey = LyConstants.INDEX_MENU_KEY;
+        if (!redisTemplate.hasKey(indexMenuKey)){
+            addRedis();
+        }
+
+        String menus = redisTemplate.opsForValue().get(indexMenuKey);
+
+        List<AllMenuDTO> allMenuDTOS = JsonUtils.toList(menus, AllMenuDTO.class);
+        return allMenuDTOS;
+    }
 }
