272e2e2f25f978ad2d577178a9f591a6636d8253
黄锦林 <5735921+huang_jin_lin@user.noreply.gitee.com>
2020-04-09 22:46:12 +0800

    完成抢购后台代码编写,增加了添加抢购商品功能2

--- ly-user/src/main/java/com/leyou/user/mapper/ProvincesMapper.java
+++ ly-user/src/main/java/com/leyou/user/mapper/ProvincesMapper.java
@@ -1,12 +1,12 @@
 package com.leyou.user.mapper;
 
 import com.leyou.common.mapper.BaseMapper;
-import com.leyou.user.entity.Provinces;
+import com.leyou.user.entity.Province;
 
 /**
  * @author Royo.Liu
  * @project_name leyou-advance
  * Created by 2020.04.09 15:15
  */
-public interface ProvincesMapper extends BaseMapper<Provinces> {
+public interface ProvincesMapper extends BaseMapper<Province> {
 }
